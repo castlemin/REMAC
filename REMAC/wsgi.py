22115a4751b05dd1320537a6dfbffa473c87c6fd
최성민 <csm1165@naver.com>
2021-08-07 07:23:24 +0900

    setting for production

--- REMAC/wsgi.py
+++ REMAC/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REMAC.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REMAC.production')
 
 application = get_wsgi_application()
