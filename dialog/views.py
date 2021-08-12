f65dd3aab03dba1943b7c5eb2b8331431049691a
최성민 <csm1165@naver.com>
2021-08-13 04:23:32 +0900

    ProductView 수정 : post 시 request_status 변경

--- dialog/views.py
+++ dialog/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from API.models import Request, Product
 from .serializers import DialogSerializer, ProductSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
 
 
 class DialogViewset(ModelViewSet):
@@ -11,3 +14,14 @@ class DialogViewset(ModelViewSet):
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = ProductSerializer(data=request.data)
+        request = Request.objects.get(id=request.data["request_id"])
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            request.request_status = 'done'
+            request.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
